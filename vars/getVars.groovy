68ac8cfb2880d4cd48381e73fb113ef24e8ea8d2
Ben Tomblin <btomblin@peeriq.com>
2023-01-03 10:05:47 -0800

    Test

---
+++ vars/getVars.groovy
@@ -0,0 +1,15 @@
+def call() {
+    return [
+        'peeriq-piq-pg01',
+        'peeriq-piq-pg02',
+        'peeriq-piq-pg03',
+        'peeriq-piq-pg04',
+
+        'peeriq-piq-dev',
+        'peeriq-piq-dev2',
+        'peeriq-piq-dev3',
+
+        'peeriq-piq-stage',
+        'peeriq-piq-prod',
+    ]
+}
