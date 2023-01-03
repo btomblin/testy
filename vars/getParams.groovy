68a233799fcb1f5eb1df38b308ccb0721c0799f5
Ben Tomblin <btomblin@peeriq.com>
2023-01-03 10:00:36 -0800

    Update getParams.groovy

--- vars/getParams.groovy
+++ vars/getParams.groovy
@@ -1,7 +1,7 @@
 def call() {
     properties([
         parameters([
-            booleanParam(defaultValue: false, name: 'BuildAll', description: 'ermahgerds')
+            booleanParam(defaultValue: false, name: 'BuildAll', description: 'ballin')
         ])
     ])
 }
